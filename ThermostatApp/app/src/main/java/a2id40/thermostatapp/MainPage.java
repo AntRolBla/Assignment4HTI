53fabd8cb0eca9721dd725126e1768333f016cb6
Antonio Roldan <antonio.rb.94@gmail.com>
2016-06-03 17:07:32 +0200

    Main page first version
    First version of the main page, with auxiliary methods for rounding the
    values, buttons added with listeners for actions, works completely.
    Included temperature constraint (no less than 5.0, no greater than 30.0)

--- ThermostatApp/app/src/main/java/a2id40/thermostatapp/MainPage.java
+++ ThermostatApp/app/src/main/java/a2id40/thermostatapp/MainPage.java
@@ -12,9 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainPage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Variables for texViews and buttons
+    TextView tv, tempBig;
+    Button button1, button2, button3, button4;
+    String s;
+
+    double temperature = 20.1; // Initialized to 20.1
+    // When complete, should take the value from the server
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,106 @@ public class MainPage extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // TextViews ID and text value
+        tv = new TextView (this);
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText("Current temperature: " + temperature + "ºC");
+        tempBig =  new TextView (this);
+        tempBig = (TextView) findViewById(R.id.tempBig);
+        tempBig.setText(temperature + "ºC");
+
+        // Adding the functions to the buttons
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temperature = round(temperature - 1.0, 1);
+                if (!checkTemperatureRange()){  // Means we have exceeded the MIN
+                    temperature = 5.0;
+                }
+                if (temperature < 10.0){ s = "0"; } else { s = ""; }    // Visual effect
+                tv.setText("Current temperature: " + s + temperature + "ºC");
+                tempBig.setText(s + temperature + "ºC");
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temperature = round(temperature - 0.1, 1);
+                if (!checkTemperatureRange()){  // Means we have exceeded the MIN
+                    temperature = 5.0;
+                }
+                if (temperature < 10.0){ s = "0"; } else { s = ""; }
+                tv.setText("Current temperature: " + s + temperature + "ºC");
+                tempBig.setText(s + temperature + "ºC");
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temperature = round(temperature + 0.1, 1);
+                if (!checkTemperatureRange()){  // Means we have exceeded the MAX
+                    temperature = 30.0;
+                }
+                if (temperature < 10.0){ s = "0"; } else { s = ""; }    // Visual effect
+                tv.setText("Current temperature: " + s + temperature + "ºC");
+                tempBig.setText(s + temperature + "ºC");
+            }
+        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temperature = round(temperature + 1.0, 1);
+                if (!checkTemperatureRange()){  // Means we have exceeded the MAX
+                    temperature = 30.0;
+                }
+                if (temperature < 10.0){ s = "0"; } else { s = ""; }    // Visual effect
+                tv.setText("Current temperature: " + s + temperature + "ºC");
+                tempBig.setText(s + temperature + "ºC");
+            }
+        });
+    }
+
+    /*
+    // Original onClick function
+
+    public void onClick(View v) {
+        if (v == button1) { // - 1 degree button
+            temperature = temperature - 1.0;
+        } else if (v == button2){ // - 0.1 degrees
+            temperature = temperature - 0.1;
+        } else if (v == button3){ // + 0.1 degrees
+            temperature = temperature + 0.1;
+        } else if (v == button4){ // + 1 degree
+            temperature = temperature + 1.0;
+        }
+        // Update texts for temperature
+        tv.setText("Current temperature: " + temperature + "ºC");
+        tempBig.setText(temperature + "ºC");
+    }
+    */
+
+    // Auxiliary method for rounding temperature to 'places' decimal (used for 1 decimal)
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    // Auxiliary method for checking if the temperature is between 5 and 30
+    public boolean checkTemperatureRange (){
+        boolean result = true;
+        if (temperature > 30.0 || temperature < 5.0){
+            result = false;
+        }
+        return result;
     }
 
     @Override
